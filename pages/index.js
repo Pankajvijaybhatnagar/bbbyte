cbf6187e2555ae79e5b3df8750dd516e9a027472
ankitjnvynr <ankitbkana@outlook.com>
2025-05-05 18:01:58 +0530

    updated by pankaj

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@ import React from 'react'
 import Hero2  from '@/components/Hero2'
 import { FeaturesSectionDemo } from '@/components/FeaturesSectionDemo';
 import { BackgroundGradient } from "@/components/BackgroundGradient";
+import HowWeWork from '@/components/HowWeWork';
+import { HoverEffect } from '@/components/HoverEffect';
+import { items } from "@/components/items";
 // OR adjust the path according to your folder structure
 
 
@@ -13,6 +16,8 @@ const index = () => {
      <Hero2/>
      <FeaturesSectionDemo/>
      <BackgroundGradient/>
+     <HowWeWork/>
+     <HoverEffect items={items} />
      
 
     </div>
